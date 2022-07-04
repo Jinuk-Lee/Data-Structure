e66dc5f2dfc1ac9cd9575579ca642af837be5a08
Jinuk Lee <20181680@edu.hanbat.ac.kr>
2022-07-04 16:52:08 +0900

    깊이 우선 탐색

---
+++ chap11/traversalDFS.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ArrayGraph.h"
+
+void traversalDFS(ArrayGraph *pGraph, int startNodeID, int *pVisit) {
+    int i = 0;
+
+    pVisit[startNodeID] = 1;
+    printf("노드-[%d] 방문 \n", startNodeID);
+    for (i = 0; i < pGraph->nodeCount; i++) {
+        if (i != startNodeID) {
+            if (0 !=getEdgeAG(pGraph, startNodeID, i)) {
+                if (0 == pVisit[i]) {
+                    traversalDFS(pGraph, i, pVisit);
+                }
+            }
+        }
+
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int nodeCount = 4;
+    ArrayGraph *pG1 = createArrayGraph(ARRAY_GRAPH_TYPE_UNDIRECT, nodeCount);
+    int *pVisit = malloc(sizeof(int)* nodeCount);
+    if(pG1 !=NULL && pVisit !=NULL){
+        addEdgeAG(pG1,0,1);
+        addEdgeAG(pG1,0,2);
+        addEdgeAG(pG1,1,3);
+        memset(pVisit,0,sizeof(int)* nodeCount);
+        printf("G1: DFS\n");
+        traversalDFS(pG1,0,pVisit);
+        deleteGraphAG(pG1);
+        free(pVisit);
+    }
+    return 0;
+}
